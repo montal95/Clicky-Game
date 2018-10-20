ca45329463f42adf75977abcc3f64faf2971e899
Sam M <sammontalvojr@gmail.com>
2018-10-19 23:19:50 -0500

    updated footer link to github repo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => (
     <footer className="footer">
         <div className="container text-center" id="footer">
-            <a  href="https://github.com/montal95/RedditScraper">GitHub Repo</a>
+            <a  href="https://github.com/montal95/Clicky-Game">GitHub Repo</a>
         </div>
     </footer>
 )
